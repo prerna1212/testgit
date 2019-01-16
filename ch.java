ed0f605a6e0f2382585e1268c1582808ec857a1c
prerna1212 <46555300+prerna1212@users.noreply.github.com>
2019-01-16 12:00:21 +0530

    Add files via upload

---
+++ ch.java
@@ -0,0 +1,13 @@
+class ch
+{
+  public static void main(String args[])
+{
+char ch1, ch2;
+ch1=10;
+ch2='Y';
+System.out.print("ch1 and ch2: ");
+System.out.println(ch1 + " " + ch2);
+ch1++;
+System.out.println("ch1 is now " + ch1);
+}
+}
